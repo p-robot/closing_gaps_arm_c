c0fa53baa9c7de2c03b8fe2918da228b03d887a2
p-robot <will@probert.co.nz>
2022-09-14 13:58:45 +0100

    Add a list of communities which are needed to be copied across (all given we take average CHiPs coverage)

--- step0a_copy_parameters.sh
+++ step0a_copy_parameters.sh
@@ -4,7 +4,7 @@ source config.sh
 
 source_dir="/well/fraser/users/probert/19_07_18_calibration_ias2019_param_limits/"
 
-for community in ${communities[@]}
+for community in ${communities_copied[@]}
 do
     cp "$source_dir/results/results_Com${community}/PARAMS_COMMUNITY${community}_ACCEPTED.tar.gz" ./parameters/
 done
@@ -13,7 +13,7 @@ echo "Parameters copied from $source_dir" >> ./parameters/README.txt
 echo "on date $(date)" >> ./parameters/README.txt
 
 
-for community in ${communities[@]}
+for community in ${communities_copied[@]}
 do
     (cd parameters; tar xzf PARAMS_COMMUNITY${community}_ACCEPTED.tar.gz)
 done
