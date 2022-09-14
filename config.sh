bc588000748a37af97da1e9e34664ff3054e3843
p-robot <will@probert.co.nz>
2022-09-14 13:58:31 +0100

    Add a list of communities which are needed to be copied across (all given we take average CHiPs coverage)

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ repo_name="IBM_simul"
 output_file="latest_commit.log"
 repo_host="p-robot"
 
-
+declare -a communities_copied=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21)
 declare -a communities=(3 4 7 12 15 17 21)  # Communities to run this analysis for
 T=2050                                # Final year of the simulation
 
